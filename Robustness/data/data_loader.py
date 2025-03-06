8b3367f61db1199fb6fbef4596b486e84fe15ec7
znbyazdani <zeinab.yazdani313@gmail.com>
2025-03-06 10:34:06 +0330

    Remove augmentation condition

--- Robustness/data/data_loader.py
+++ Robustness/data/data_loader.py
@@ -4,14 +4,13 @@ import torchvision.transforms as transforms
 import math
 
 
-def load_data(batch_size=64, val_split=0.8, data_path='.', augmentation=False):
+def load_data(batch_size=64, val_split=0.8, data_path='.'):
 
     # Define transforms
-    if augmentation:
-        train_transform = transforms.compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.229, 0.224, 0.225]),
-        ])
+    train_transform = transforms.compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.229, 0.224, 0.225]),
+    ])
     test_transform = transforms.compose([
             transforms.ToTensor(),
     ])
