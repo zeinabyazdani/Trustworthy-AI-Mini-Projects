7257e1ebc4f782032350f1c3740970d1e9f81540
znbyazdani <zeinab.yazdani313@gmail.com>
2025-03-11 12:57:16 +0330

    Add sampler for stratified train-test split

--- Robustness/data/data_loader.py
+++ Robustness/data/data_loader.py
@@ -1,30 +1,38 @@
+import torch
 import torchvision
 from torch.utils.data import DataLoader, random_split
 import torchvision.transforms as transforms
+import torch.utils.data as data
+import numpy as np
 import math
 
 
 def load_data(batch_size=64, val_split=0.8, data_path='.'):
 
     # Define transforms
-    train_transform = transforms.compose([
+    transform = transforms.compose([
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.229, 0.224, 0.225]),
-    ])
-    test_transform = transforms.compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.229, 0.224, 0.225]),
+        # transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.229, 0.224, 0.225]),
     ])
 
-    train_ds = torchvision.datasets.CIFAR10(root=data_path, train=True, transform=train_transform, download=True)
-    test_ds  =torchvision.datasets.CIFAR10(root=data_path, train=False, transform=test_transform,download=True)
+    # Load CIFAR-10 dataset
+    train_ds = torchvision.datasets.CIFAR10(root=data_path, train=True, transform=transform, download=True)
+    test_ds  = torchvision.datasets.CIFAR10(root=data_path, train=False, transform=transform,download=True)
+
+    # Split train and validation datasets
+    indices = np.arange(len(train_ds))
+    np.random.shuffle(indices)
     val_size = math.floor(len(train_ds) * val_split)
-    tr_size  = len(train_ds) * val_size
-    train_ds, val_ds = random_split(train_ds, [tr_size, val_size])
-    print(f"train_ds: {len(train_ds)}, \nsvhn_test_ds: {len(test_ds)}, \nvalidation_ds: {len(val_ds)}")
+    train_idx, val_idx = indices[val_size:], indices[:val_size]
+    
+    train_sampler = data.SubsetRandomSampler(train_idx)
+    val_sampler   = data.SubsetRandomSampler(val_idx)
 
-    train_loader = DataLoader(train_ds, batch_size, shuffle=True)
-    val_loader   = DataLoader(val_ds, batch_size, shuffle=True)
+    # print(f"train_ds: {len(train_ds)}, \nsvhn_test_ds: {len(test_ds)}, \nvalidation_ds: {len(val_ds)}")
+
+    train_loader = DataLoader(train_ds, batch_size, sampler=train_sampler)
+    val_loader   = DataLoader(train_ds, batch_size, sampler=val_sampler)
     test_loader  = DataLoader(test_ds, batch_size, shuffle=True)
 
     return train_loader, val_loader, test_loader
+
